7545f9f5541eb5d5d4188bcb562515a444b63100
SangWonKo <swko1017@naver.com>
2022-08-14 17:52:27 +0900

    fetch news contents by hashchange event listening

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -1,5 +1,10 @@
 const ajax = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/jobs/1.json";
+const CONTENTS_URL = "https://api.hnpwa.com/v0/item/@id.json";
+
+const container = document.getElementById("root");
+const content = document.createElement("div");
+
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
@@ -7,10 +12,31 @@ const newsFeed = JSON.parse(ajax.response);
 
 const ul = document.createElement("ul");
 
+//해쉬 변경됨을 인식하여 클릭된 뉴스 컨텐츠 fetching 
+window.addEventListener("hashchange", function() {
+  console.log("해쉬 변경됨");
+  const id = this.location.hash.substring(1);
+  ajax.open("GET", CONTENTS_URL.replace('@id', id), false);
+  ajax.send();
+
+  const newsContents = JSON.parse(ajax.response);
+  console.log(newsContents);
+
+  const title = this.document.createElement("h1");
+
+  title.innerHTML = newsContents.title;
+  content.appendChild(title);
+});
+
 for (let i = 0; i < newsFeed.length; i++) {
   const li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement("a");
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} ${newsFeed[i].comments_count}`;
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
