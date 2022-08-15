94779f9ae9af9be8d8515c93c1fdc2246a20ec3e
SangWonKo <swko1017@naver.com>
2022-08-15 17:06:19 +0900

    hashchange routing 적용

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -3,7 +3,7 @@ const NEWS_URL = "https://api.hnpwa.com/v0/jobs/1.json";
 const CONTENTS_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 const container = document.getElementById("root");
-const content = document.createElement("div");
+// const content = document.createElement("div");
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -12,34 +12,48 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-//해쉬 변경됨을 인식하여 클릭된 뉴스 컨텐츠 fetching
-window.addEventListener("hashchange", function () {
-  console.log("해쉬 변경됨");
-  const id = this.location.hash.substring(1);
-  const newsContents = getData(CONTENTS_URL.replace("@id", id));
-
-  console.log(newsContents);
-
-  const title = this.document.createElement("h1");
-
-  title.innerHTML = newsContents.title;
-  content.appendChild(title);
-});
-
-for (let i = 0; i < newsFeed.length; i++) {
-  const div = document.createElement("div");
-  div.innerHTML = `
+  newsList.push("<ul>");
+  for (let i = 0; i < newsFeed.length; i++) {
+    newsList.push(`
     <li>
       <a href="#${newsFeed[i].id}">
         ${newsFeed[i].title} ${newsFeed[i].comments_count}
       </a>
     </li>
-  `;
-  ul.appendChild(div.firstElementChild);
+  `);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = location.hash.substring(1); //# 제거
+  const newsContents = getData(CONTENTS_URL.replace("@id", id));
+
+  container.innerHTML = `
+    <h1>${newsContents.title}</h1>
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+  console.log(routePath);
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
