c5f8883d6fe4b1f41b01e381efdbb56a1e928335
SangWonKo <swko1017@naver.com>
2022-08-15 17:50:03 +0900

    페이징 구현

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -4,6 +4,9 @@ const CONTENTS_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 const container = document.getElementById("root");
 // const content = document.createElement("div");
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -17,10 +20,10 @@ function newsFeed() {
   const newsList = [];
 
   newsList.push("<ul>");
-  for (let i = 0; i < newsFeed.length; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li>
-      <a href="#${newsFeed[i].id}">
+      <a href="#/show/${newsFeed[i].id}">
         ${newsFeed[i].title} ${newsFeed[i].comments_count}
       </a>
     </li>
@@ -29,17 +32,30 @@ function newsFeed() {
 
   newsList.push("</ul>");
 
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1 //이전페이지 방어코드
+      }">이전 페이지</a>
+      <a href="#/page/${
+        store.currentPage * 10 >= newsFeed.length
+          ? store.currentPage
+          : store.currentPage + 1
+      }">다음 페이지</a>
+    </div>
+  `);
+
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  const id = location.hash.substring(1); //# 제거
+  const id = location.hash.substring(7); //# 제거
   const newsContents = getData(CONTENTS_URL.replace("@id", id));
 
   container.innerHTML = `
     <h1>${newsContents.title}</h1>
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -48,6 +64,9 @@ function router() {
   const routePath = location.hash;
   console.log(routePath);
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
